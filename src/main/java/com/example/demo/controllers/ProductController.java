0122c8cecb4b98615db468dea57451c771ef44a3
ThierryTe <kimathierry@gmail.com>
2023-05-31 01:42:58 +0200

    Ajout de RestController

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @ResponseBody
-@Controller
+@RestController
 public class ProductController {
 
     private final ProductRepository productRepository;
